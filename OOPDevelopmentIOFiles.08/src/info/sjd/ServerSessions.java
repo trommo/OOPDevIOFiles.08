ea2618707c5959c66994d103eff74a727d8017ec
trommo <ot3@ukr.net>
2018-11-16 22:42:33 +0200

    Writing to file is realized.

--- OOPDevelopmentIOFiles.08/src/info/sjd/ServerSessions.java
+++ OOPDevelopmentIOFiles.08/src/info/sjd/ServerSessions.java
@@ -3,6 +3,9 @@ package info.sjd;
 import info.sjd.service.*;
 import info.sjd.SessionData;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +25,37 @@ public class ServerSessions {
 				sessions.add(session);
 			}
 
-			
+		File file = new File("src/info/sjd/log/SessionLog.txt");	
 		
-		for (int i = 0; i < limit; i++) {
-			System.out.println(sessions.get(i).getSessionStartTime() + " " + sessions.get(i).getSessionID() + " " + sessions.get(i).getSessionIP());
+		try {
+			FileWriter writer = new FileWriter(file, true);
+			for (int i = 0; i < limit; i++) {
+				writer.write(sessions.get(i).getSessionStartTime() + " " + sessions.get(i).getSessionID() + " " + sessions.get(i).getSessionIP() + "\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+//		if (file.exists()) {
+//			System.out.print("Cool!");
+//		} else {
+//			try {
+//				boolean created = file.createNewFile();
+//				if (created)
+//					System.out.println("File has been created!");
+//			} catch (IOException e) {
+//				
+//				e.printStackTrace();
+//			}
+//			System.out.println("File not exist");
+//		}
+		
+//		for (int i = 0; i < limit; i++) {
+//			System.out.println(sessions.get(i).getSessionStartTime() + " " + sessions.get(i).getSessionID() + " " + sessions.get(i).getSessionIP());
+//		}
 		
 	}
 	
